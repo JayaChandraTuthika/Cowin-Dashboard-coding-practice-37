5bcc38d9f43a0682af0f35a6abd5789fba991429
JayaChandraTuthika <122348011+JayaChandraTuthika@users.noreply.github.com>
2023-02-19 18:05:55 +0530

    Update index.js

--- cowinDashboard/src/components/VaccinationByGender/index.js
+++ cowinDashboard/src/components/VaccinationByGender/index.js
@@ -1,4 +1,4 @@
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 import './index.css'
 
@@ -8,36 +8,35 @@ const VaccinationByGender = props => {
   return (
     <>
       <h1 className="chart-heading">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={400}>
-        <PieChart>
-          <Pie
-            cx="50%"
-            cy="50%"
-            data={data}
-            startAngle={180}
-            endAngle={0}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill="#f54394" />
-            <Cell name="Female" fill="#5a8dee" />
-            <Cell name="Others" fill="#2cc6c6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-            wrapperStyle={{
-              padding: 0,
-              fontFamily: 'Roboto',
-              fontSize: '17px',
-              fontWeight: '600',
-            }}
-          />
-        </PieChart>
-      </ResponsiveContainer>
+
+      <PieChart width={1000} height={400}>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={data}
+          startAngle={180}
+          endAngle={0}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{
+            padding: 0,
+            fontFamily: 'Roboto',
+            fontSize: '17px',
+            fontWeight: '600',
+          }}
+        />
+      </PieChart>
     </>
   )
 }
